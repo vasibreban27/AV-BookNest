55bf35e18f0ea6bb0755f281f0852ccd6646b6d1
Vasi Breban <vbreban09@gmail.com>
2026-07-11 16:03:29 +0300

    resolved connection to neon postgres db and flyway works

--- backend/src/main/java/com/avbooknest/BackendApplication.java
+++ backend/src/main/java/com/avbooknest/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.avbooknest;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +12,4 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
